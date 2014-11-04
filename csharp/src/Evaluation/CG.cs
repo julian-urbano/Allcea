b3bc437c6856d4613643ea4d6d4b36945bae704a
julian-urbano <urbano.julian@gmail.com>
2014-11-04 17:49:40 +0100

    Added CG measure and confidence estimator with Normal distribution.

---
+++ csharp/src/Evaluation/CG.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (C) 2014  Julián Urbano <urbano.julian@gmail.com>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using jurbano.Allcea.Model;
+using jurbano.Allcea.Estimation;
+
+namespace jurbano.Allcea.Evaluation
+{
+    [global::System.Diagnostics.DebuggerDisplay("MaxRelevance={MaxRelevance}")]
+    public class CG : IMeasure
+    {
+        public int MaxRelevance { get; protected set; }
+
+        public CG(int maxrelevance)
+        {
+            if (maxrelevance < 1) {
+                throw new ArgumentException("The maximum relevance level cannot be less than 1.");
+            }
+            this.MaxRelevance = maxrelevance;
+        }
+
+        public RelativeEffectivenessEstimate Estimate(Run runA, Run runB, IRelevanceEstimator estimator)
+        {
+            double e = 0, var = 0;
+
+            HashSet<string> inRunA = new HashSet<string>(); // retrieved by run A
+            foreach (string doc in runA.Documents) {
+                RelevanceEstimate docEst = estimator.Estimate(runA.Query, doc);
+                e += docEst.Expectation;
+                var += docEst.Variance;
+                inRunA.Add(doc);
+            }
+            foreach (string doc in runB.Documents) {
+                RelevanceEstimate docEst = estimator.Estimate(runB.Query, doc);
+                e -= docEst.Expectation;
+                if (inRunA.Contains(doc)) {
+                    // If retrieved in both runs, does not contribute to variance
+                    var -= docEst.Variance;
+                } else {
+                    var += docEst.Variance;
+                }
+            }
+            e /= inRunA.Count;
+            var /= inRunA.Count * inRunA.Count;
+
+            e /= this.MaxRelevance;
+            var /= this.MaxRelevance * this.MaxRelevance;
+
+            return new RelativeEffectivenessEstimate(runA.System, runB.System, runA.Query, e, var);
+        }
+    }
+}
---
+++ csharp/src/Evaluation/NormalConfidenceEstimator.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (C) 2014  Julián Urbano <urbano.julian@gmail.com>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using jurbano.Allcea.Model;
+
+namespace jurbano.Allcea.Evaluation
+{
+    public class NormalConfidenceEstimator : IConfidenceEstimator
+    {
+        public double[] EstimateInterval(RelativeEffectivenessEstimate estimate, double confidence)
+        {
+            double z = NormalConfidenceEstimator.Quantile((1.0 - confidence) / 2.0);
+            double len = Math.Abs(z * Math.Sqrt(estimate.Variance));
+            return new double[] { estimate.Expectation - len, estimate.Expectation + len };
+        }
+        public double EstimateConfidence(RelativeEffectivenessEstimate estimate)
+        {
+            double conf = NormalConfidenceEstimator.CDF(estimate.Expectation / Math.Sqrt(estimate.Variance));
+            return conf;// Math.Max(conf, 1 - conf);
+        }
+
+        protected static double CDF(double x)
+        {
+            // From http://www.johndcook.com/csharp_phi.html
+
+            // constants
+            double a1 = 0.254829592;
+            double a2 = -0.284496736;
+            double a3 = 1.421413741;
+            double a4 = -1.453152027;
+            double a5 = 1.061405429;
+            double p = 0.3275911;
+
+            // Save the sign of x
+            int sign = 1;
+            if (x < 0)
+                sign = -1;
+            x = Math.Abs(x) / Math.Sqrt(2.0);
+
+            // A&S formula 7.1.26
+            double t = 1.0 / (1.0 + p * x);
+            double y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.Exp(-x * x);
+
+            return 0.5 * (1.0 + sign * y);
+        }
+        protected static double Quantile(double p)
+        {
+            // From http://www.johndcook.com/csharp_phi_inverse.html
+
+            if (p <= 0.0 || p >= 1.0) {
+                string msg = String.Format("Invalid input argument: {0}.", p);
+                throw new ArgumentOutOfRangeException(msg);
+            }
+
+            // See article above for explanation of this section.
+            if (p < 0.5) {
+                // F^-1(p) = - G^-1(p)
+                return -NormalConfidenceEstimator.RationalApproximation(Math.Sqrt(-2.0 * Math.Log(p)));
+            } else {
+                // F^-1(p) = G^-1(1-p)
+                return NormalConfidenceEstimator.RationalApproximation(Math.Sqrt(-2.0 * Math.Log(1.0 - p)));
+            }
+        }
+        protected static double RationalApproximation(double t)
+        {
+            // Abramowitz and Stegun formula 26.2.23.
+            // The absolute value of the error should be less than 4.5 e-4.
+            double[] c = { 2.515517, 0.802853, 0.010328 };
+            double[] d = { 1.432788, 0.189269, 0.001308 };
+            return t - ((c[2] * t + c[1]) * t + c[0]) /
+                        (((d[2] * t + d[1]) * t + d[0]) * t + 1.0);
+        }
+    }
+}
